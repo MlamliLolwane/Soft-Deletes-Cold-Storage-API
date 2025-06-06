681ddcdf7611c9db6bc475aafbd82a2ec0fb0a51
Mlamli Lolwane <mrlolwane96@gmail.com>
2025-06-06 03:28:29 +0200

    chore: edit routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArchivedStatusController;
 use App\Http\Controllers\StatusController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
@@ -11,7 +12,12 @@ Route::post('/register', [UserController::class, 'register']);
 
 Route::apiResource('status', StatusController::class);
 Route::put('/status/restore/{id}', [StatusController::class, 'restore']);
+Route::delete('/status/permanent/delete/{id}', [StatusController::class, 'permanently_delete']);
 
 
 Route::apiResource('task', TaskController::class);
 Route::put('/task/restore/{id}', [TaskController::class, 'restore']);
+
+Route::apiResource('archived-status', ArchivedStatusController::class)->except(['update']);
+Route::delete('/archived-status/restore/{id}', [ArchivedStatusController::class, 'restore']);
+Route::delete('/archived-status/permanent/delete/{id}', [ArchivedStatusController::class, 'permanently_delete']);
