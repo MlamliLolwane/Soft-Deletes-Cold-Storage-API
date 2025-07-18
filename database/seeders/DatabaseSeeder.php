4c0cb5773b3c2049aecacebed6d24068710b0815
Mlamli Lolwane <53498225+MlamliLolwane@users.noreply.github.com>
2025-07-18 06:19:58 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Status::create(['status' => 'Pending');
+        Status::create(['status' => 'Pending']);
     }
 }
